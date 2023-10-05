ef4b4d3b458a2c8a05d8e3a9e25fd520e76a59b9
Oche David Ankeli <o.ankeli@alustudent.com>
2023-10-05 20:11:44 +0100

    Update 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -2,35 +2,36 @@
 
 def rain(walls):
     """
-    Calculate the total amount of rainwater retained between walls.
+    Calculate how many square units of water will be retained after it rains.
 
-    Args:
-        walls (List[int]): A list of non-negative integers representing wall heights.
-
-    Returns:
-        int: Total amount of rainwater retained.
+    :param walls: A list of non-negative integers representing wall heights.
+    :return: Integer indicating the total amount of rainwater retained.
     """
+
     if not walls:
         return 0
 
-    n = len(walls)
-    left_max = [0] * n
-    right_max = [0] * n
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
 
-    left_max[0] = walls[0]
-    for i in range(1, n):
-        left_max[i] = max(left_max[i - 1], walls[i])
+    while left < right:
+        if walls[left] <= walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
 
-    right_max[n - 1] = walls[n - 1]
-    for i in range(n - 2, -1, -1):
-        right_max[i] = max(right_max[i + 1], walls[i])
+    return water
 
-    total_water = 0
-    for i in range(n):
-        total_water += min(left_max[i], right_max[i]) - walls[i]
-
-    return total_water
-
+# Test cases
 if __name__ == "__main__":
     walls1 = [0, 1, 0, 2, 0, 3, 0, 4]
     print(rain(walls1))  # Output: 6
