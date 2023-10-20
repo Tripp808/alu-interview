9d32d147e0f3545e1f3692e4f9abd4e1ca64d9d8
Oche David Ankeli <o.ankeli@alustudent.com>
2023-10-20 20:36:28 +0100

    pep8 validation 0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 0-main
 """
@@ -8,24 +7,15 @@ def pascal_triangle(n):
     """
     Generate Pascal's Triangle up to the nth row.
 
-    This function generates Pascal's Triangle, a mathematical pattern where each
-    number is the sum of the two numbers directly above it. The function returns
-    a list of lists representing the rows of the triangle.
-
     Args:
-        n (int): The number of rows to generate. Must be a non-negative integer.
+        n (int): The number of rows to generate.
 
     Returns:
-        List[List[int]]: A list of lists representing the Pascal's Triangle.
+        List[List[int]]: A list of lists representing Pascal's Triangle.
 
     Example:
         >>> pascal_triangle(5)
         [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
-
-    :param n: The number of rows to generate in Pascal's Triangle.
-    :type n: int
-    :return: A list of lists representing Pascal's Triangle.
-    :rtype: List[List[int]]
     """
     if n <= 0:
         return []
